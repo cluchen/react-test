5878ea6a162cb3148f4a25c72db9f76d279dfc0a
cynthia <cluchen@ucdavis.edu>
2020-05-24 00:50:33 -0700

    final

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Tweet from './Tweet';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [users, setUser] = useState ([
+    {name: 'Ed', message: 'Hello there'},
+    {name: 'John', message: 'I am jon snow'},
+    {name: 'traersy', message: 'im awesome'}
+  ]);
+  /*const [isRed, setRed] = useState(false);
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count +1);
+    setRed(!isRed);
+  };*/
+
+  return(
+    <div className="app">
+      {users.map(user => (
+        <Tweet name={user.name} message = {user.message}/>
+      ))}
     </div>
   );
 }
